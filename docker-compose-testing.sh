ee314c4a65cc62f3c2bc0215be5f23b15b1b3ee2
nikesh-mahalka-2 <38130007+nikesh-mahalka-2@users.noreply.github.com>
2018-05-26 01:00:11 +0530

    Create docker-compose-testing.sh

---
+++ docker-compose-testing.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+chmod +x /workspace/check_oracle_table.sh
+
+# To get access to docker store
+echo "docker login"
+docker login -u nike1niec -p $DOCKER_LOGIN
+
+# Oracle database service
+echo "docker-compose up -d dbserver" 
+docker-compose up -d dbserver
+
+echo "checking Oracle database health to be healthy"
+
+DB_HEALTH=""
+while [ "${DB_HEALTH}" != "\"healthy\"" ]
+do
+  DB_HEALTH="$(docker inspect --format='{{json .State.Health.Status}}' dbserver)"
+  sleep 20
+  echo $DB_HEALTH
+done
+
+# Application service: ojdbc7.jar dependency will be installed in local maven repo, maven source code will be build and
+# application will be run against the Oracle Database
+echo "running docker-compose up appserver"
+docker-compose up appserver
+
+# Checking existence of table app_server in Oracle Database created by application
+sudo docker exec dbserver /workspace/check_oracle_table.sh
+
+docker-compose down
