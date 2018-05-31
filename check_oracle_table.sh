75f2fc67df42d51c102fb069bcedb742921df856
manikantanr <manikantanr@biarca.com>
2018-05-31 19:00:26 +0530

    add

--- check_oracle_table.sh
+++ check_oracle_table.sh
@@ -1,11 +1,23 @@
 #!/bin/bash
-VAR="ERROR: ORA-"
-while [[ $VAR =~ "ERROR: ORA-" ]]
+STATUS="ERROR: ORA-"
+num_retry=1
+echo "checking existence of app_message"
+until [ $num_retry -gt $DB_TBL_CHK_MAX_RETRY ]
 do
-  echo "checking existence of app_message"
-  VAR=`/u01/app/oracle/product/12.2.0/dbhome_1/bin/sqlplus -S sys/Oradoc_db1@dbserver.workspace_default:1521/ORCLCDB.localdomain as sysdba   << EOF
+  STATUS=`/u01/app/oracle/product/12.2.0/dbhome_1/bin/sqlplus -S sys/Oradoc_db1@dbserver.workspace_default:1521/ORCLCDB.localdomain as sysdba << EOF
 describe app_message
 EOF`
-  sleep 10
+  if [[ $STATUS =~ "ERROR: ORA-" ]]; then
+    echo "app_message table creation status is: $STATUS"
+    echo "retry-$num_retry to check app_message table creation status"
+    num_retry=$[$num_retry+1]
+    sleep $SLEEP
+  else
+    break
 done
-echo "app_message table is created"
+
+if [[ $STATUS =~ "ERROR: ORA-" ]]; then
+  echo "app_message table creation status is: $STATUS"
+  exit 1
+else
+  echo "app_message table creation status is: $STATUS"
