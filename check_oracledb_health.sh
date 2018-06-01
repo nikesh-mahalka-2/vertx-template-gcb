6ff994cba7e7988b657456b0dc0507337700bc93
nikesh-mahalka-2 <38130007+nikesh-mahalka-2@users.noreply.github.com>
2018-06-01 22:19:03 +0530

    Update check_oracledb_health.sh

--- check_oracledb_health.sh
+++ check_oracledb_health.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 set -xe
-num_retry=1
-until [ $num_retry -gt $DB_HLTH_CHK_MAX_RETRY ]
+num_retry=0
+until [[ $num_retry -gt $DB_HLTH_CHK_MAX_RETRY ]]
 do
   DB_HEALTH="$(docker inspect --format='{{json .State.Health.Status}}' dbserver)"
 	if [[ "${DB_HEALTH}" == "\"healthy\"" ]]; then
@@ -9,8 +9,13 @@ do
     break
 	else
     echo "Oracle DB is $DB_HEALTH"
+    num_retry=$[$num_retry+1]
 	  echo "retry-$num_retry to check health of Oracle DB"
-	  num_retry=$[$num_retry+1]
+	  
   fi
   sleep $DB_HLTH_CHK_SLEEP
 done
+
+if [[ $num_retry -gt $DB_HLTH_CHK_MAX_RETRY ]]
+  exit 1
+fi
