ee7a4565fb99c5a3f24d07cddab97c26ef4a82e5
nikesh-mahalka-2 <38130007+nikesh-mahalka-2@users.noreply.github.com>
2018-06-02 17:16:33 +0530

    Update app_build.sh

--- app_build.sh
+++ app_build.sh
@@ -5,4 +5,4 @@ cd /workspace
 mvn install:install-file -Dfile=./ojdbc7.jar -DgroupId=com.oracle.jdbc -DartifactId=ojdbc7 -Dversion=12.1.0.2 -Dpackaging=jar
 mvn -DskipTests clean package
 curl -sSL https://download.sourceclear.com/ci.sh | sh;
-nohup java -Ddatabase.url=jdbc:oracle:thin:@dbserver.workspace_default:1521/ORCLCDB.localdomain -Djava.security.egd=file:/dev/./urandom -jar ./target/vertx-*-SNAPSHOT.jar create-database run 2>&1 &
+nohup java -Ddatabase.url=jdbc:oracle:thin:@dbserver:1521/ORCLCDB.localdomain -Djava.security.egd=file:/dev/./urandom -jar ./target/vertx-*-SNAPSHOT.jar create-database run 2>&1 &
