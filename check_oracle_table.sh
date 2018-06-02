89ff9cfe24d7f2db41a991e81d705ec11520b85d
nikesh-mahalka-2 <38130007+nikesh-mahalka-2@users.noreply.github.com>
2018-06-02 14:38:54 +0530

    Update check_oracle_table.sh

--- check_oracle_table.sh
+++ check_oracle_table.sh
@@ -1,30 +1,21 @@
 #!/bin/bash
-
+set -xe
 SQLPATH="/u01/app/oracle/product/12.2.0/dbhome_1/bin/sqlplus"
 LOGON="sys/Oradoc_db1@dbserver.workspace_default:1521/ORCLCDB.localdomain as sysdba"
 
-num_retry=1
-
-echo "Checking existence of table app_server in Oracle DB created by the application"
-
+num_retry=0
 until [[ $num_retry -gt $DB_TBL_CHK_MAX_RETRY ]]
 do
-  RES=`$SQLPATH -S $LOGON << EOF
-describe app_message
-EOF`
-  if [[ $RES =~ "ERROR:" ]]; then
-    echo "app_message table creation status is: $RES"
-    echo "retry-$num_retry to check app_message table creation status"
-    num_retry=$[$num_retry+1]
-    sleep $DB_TBL_CHK_SLEEP
-  else
-    break
+  echo "retry-$num_retry to check existence of table app_server in Oracle DB"
+  num_retry=$[$num_retry+1]
+  RESP=`$SQLPATH -S $LOGON << EOF
+  describe app_message
+  EOF`
+  echo "app_message table creation status is: $RESP"
+  if [[ $RESP != *"ERROR:"* ]]; then
+    exit 0
   fi
+  sleep $DB_TBL_CHK_SLEEP
 done
 
-echo "app_message table creation status is: $RES"
-if [[ $RES =~ "ERROR:" ]]; then
-  exit 1
-fi
-
-exit 0
+exit 1
