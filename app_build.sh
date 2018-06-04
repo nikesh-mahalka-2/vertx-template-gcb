3a2ae42163d0c3bc5c75a3282fdcee99ac8e0e54
root <root@instance-2.c.spinnaker-poc-200412.internal>
2018-06-04 13:12:50 +0000

    test

--- app_build.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-apt-get update
-apt-get install curl -y
-cd /workspace
-mvn install:install-file -Dfile=./ojdbc7.jar -DgroupId=com.oracle.jdbc -DartifactId=ojdbc7 -Dversion=12.1.0.2 -Dpackaging=jar
-mvn -DskipTests clean package
-curl -sSL https://download.sourceclear.com/ci.sh | sh;
