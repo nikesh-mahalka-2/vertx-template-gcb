536b39d356d99172dead6f2c0bc8340cbfc7b33a
root <root@instance-2.c.spinnaker-poc-200412.internal>
2018-06-04 10:35:20 +0000

    tets

--- check_oracledb_health.sh
+++ check_oracledb_health.sh
@@ -1,17 +1,16 @@
 #!/bin/bash
 set -xe
 num_retry=0
-until [[ $num_retry -gt $DB_HLTH_CHK_MAX_RETRY ]]
+until [[ "$num_retry" -gt "$DB_HLTH_CHK_MAX_RETRY" ]]
 do
   echo "retry-$num_retry to check health of Oracle DB"
-  num_retry=$[$num_retry+1]
-  DB_HEALTH="$(docker inspect --format='{{json .State.Health.Status}}' $ORACLEDB_SERVER)"
+  num_retry=$((num_retry+1))
+  DB_HEALTH="$(docker inspect --format='{{json .State.Health.Status}}' "$ORACLEDB_SERVER")"
   echo "Oracle DB is $DB_HEALTH"
-  
-	if [[ "${DB_HEALTH}" == "\"healthy\"" ]]; then
+  if [[ "${DB_HEALTH}" == "\"healthy\"" ]]; then
     exit 0
   fi
-  sleep $DB_HLTH_CHK_SLEEP
+  sleep "$DB_HLTH_CHK_SLEEP"
 done
 
 exit 1
--- check_oracledb_table.sh
+++ check_oracledb_table.sh
@@ -4,18 +4,19 @@ SQLPATH="/u01/app/oracle/product/12.2.0/dbhome_1/bin/sqlplus"
 LOGON="sys/Oradoc_db1@$ORACLEDB_SERVER:1521/ORCLCDB.localdomain as sysdba"
 
 num_retry=0
-until [[ $num_retry -gt $DB_TBL_CHK_MAX_RETRY ]]
+until [[ "$num_retry" -gt "$DB_TBL_CHK_MAX_RETRY" ]]
 do
   echo "retry-$num_retry to check existence of table app_server in Oracle DB"
-  num_retry=$[$num_retry+1]
-  RESP=`$SQLPATH -S $LOGON << EOF
+  num_retry=$((num_retry+1))
+  RESP=$("$SQLPATH" -S "$LOGON" << EOF
 describe app_message
-EOF`
+EOF
+)
   echo "app_message table creation status is: $RESP"
-  if [[ $RESP != *"ERROR:"* ]]; then
+  if [[ "$RESP" != *"ERROR:"* ]]; then
     exit 0
   fi
-  sleep $DB_TBL_CHK_SLEEP
+  sleep "$DB_TBL_CHK_SLEEP"
 done
 
 exit 1
