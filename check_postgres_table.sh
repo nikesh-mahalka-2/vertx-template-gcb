23d728e0dd24fe6a61bd27b91444fe22212d8dbb
nikesh-mahalka-2 <38130007+nikesh-mahalka-2@users.noreply.github.com>
2018-06-05 02:12:35 +0530

    Update check_postgres_table.sh

--- check_postgres_table.sh
+++ check_postgres_table.sh
@@ -2,7 +2,7 @@
 num_retry=0
 until [[ "$num_retry" -gt "$DB_TBL_CHK_MAX_RETRY" ]]
 do
-  echo "retry-$num_retry to check existence of table app_server in Postgres DB"
+  echo "retry-$num_retry to check existence of table app_message in Postgres DB"
   num_retry=$((num_retry+1))
   RESP=$(PGPASSWORD="$POSTGRES_PASSWORD" psql -U "$POSTGRES_USER" -d "$POSTGRES_DB" -h "$POSTGRES_SERVER" -c '\d app_message' > /dev/null 2>&1; echo $?)
   if [[ "$RESP" == 0 ]]; then
